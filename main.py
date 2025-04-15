53b4d978b50f3212b1bcec0a8fa6e2d5e3726bcc
Annalena Kreischer <a.kreischer@online.de>
2025-04-15 18:20:47 +0200

    put greeting into function

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello from Hangman")
+def main():
+    print("Hello from Hangman")
+    #secret_word = input("Bitte gib ein Wort ein: ").lower()
+    #print(f"Das geheime Wort hat {len(secret_word)} Buchstaben.")
+
+main()

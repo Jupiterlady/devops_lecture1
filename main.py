f8ada0fa79ba9b9e5a2e379452cb3a99d423e8a3
Annalena Kreischer <a.kreischer@online.de>
2025-04-15 18:22:02 +0200

    print word length in main function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def main():
     print("Hello from Hangman")
     secret_word = input("Bitte gib ein Wort ein: ").lower()
-    #print(f"Das geheime Wort hat {len(secret_word)} Buchstaben.")
+    print(f"Das geheime Wort hat {len(secret_word)} Buchstaben.")
 
 main()

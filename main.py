e5b777b8d7b082a633ebcc3997a242b575500777
Annalena Kreischer <a.kreischer@online.de>
2025-04-16 10:44:14 +0200

    first guess added to main()

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ def main():
     print("Hello from Hangman")
     secret_word = input("Bitte gib ein Wort ein: ").lower()
     print(f"Das geheime Wort hat {len(secret_word)} Buchstaben.")
+    guess = input("Rate einen Buchstaben: ").lower()
+    print(f"Du hast '{guess}' geraten.")
 
 if __name__ == "__main__":
     main()

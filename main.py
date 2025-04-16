56f6352efc5ce3c823d7a245fc3b893fcc28ac46
Annalena Kreischer <a.kreischer@online.de>
2025-04-16 11:13:15 +0200

    remove initial guess input outside game

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ def main():
     print("Hello from Hangman")
     secret_word = input("Bitte gib ein Wort ein: ").lower()
     print(f"Das geheime Wort hat {len(secret_word)} Buchstaben.")
-    guess = input("Rate einen Buchstaben: ").lower()
-    print(f"Du hast '{guess}' geraten.")
 
     guessed_letters = set()
 
